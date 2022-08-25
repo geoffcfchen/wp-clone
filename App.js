31d9917cdca65b5f9becb0f878c4e837b7cc2e3d
chinfanster <chinfanster@gmail.com>
2022-08-25 14:44:42 -0700

    ignore the warning

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, LogBox } from "react-native";
 import { useAssets } from "expo-asset";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "./firebase";
+
+// this is the long issue for the firestore
+LogBox.ignoreLogs([
+  "Setting a timer",
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release.",
+]);
 
 function App() {
   const [currUser, setCurrUser] = useState(null);
@@ -19,9 +25,13 @@ function App() {
     return () => unsubscribe();
   });
 
+  if (loading) {
+    return <Text>Loading</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>test!</Text>
+      <Text>{JSON.stringify(currUser)}</Text>
       <StatusBar style="auto" />
     </View>
   );
