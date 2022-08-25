97dc7e4142d6adc9dd4fee807086157a2feab72e
chinfanster <chinfanster@gmail.com>
2022-08-25 13:50:39 -0700

    set up firebase.js

---
+++ firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlYEAHVWYmDghYf1sHWap9c8UFAKlSYNc",
+  authDomain: "wp-clone-256aa.firebaseapp.com",
+  projectId: "wp-clone-256aa",
+  storageBucket: "wp-clone-256aa.appspot.com",
+  messagingSenderId: "979935378743",
+  appId: "1:979935378743:web:c2ca33520c51cc8dbdaeb2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
