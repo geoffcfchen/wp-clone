a5cdad3a4f55279f1fc89a00bb86fbfa12463bbe
chinfanster <chinfanster@gmail.com>
2022-08-30 12:55:55 -0700

    fix bug

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,5 +1,11 @@
 // @refresh reset
-import { View, Text, ImageBackground } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 import "react-native-get-random-values";
 import { nanoid } from "nanoid";
 import React, { useContext, useState, useEffect, useCallback } from "react";
@@ -14,13 +20,22 @@ import {
   setDoc,
   addDoc,
 } from "firebase/firestore";
-import { GiftedChat } from "react-native-gifted-chat";
-
+import {
+  Actions,
+  Bubble,
+  GiftedChat,
+  InputToolbar,
+} from "react-native-gifted-chat";
+import { Ionicons } from "@expo/vector-icons";
+import { pickImage, uploadImage } from "../utils";
+import ImageView from "react-native-image-viewing";
 const randomID = nanoid();
 
 export default function Chat() {
   const [roomHash, setRoomHash] = useState("");
   const [messages, setMessages] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedImageView, setSelectedImageView] = useState("");
   const {
     theme: { colors },
   } = useContext(GlobalContext);
@@ -82,7 +97,8 @@ export default function Chat() {
         .map(({ doc }) => {
           const message = doc.data();
           return { ...message, createdAt: message.createdAt.toDate() };
-        });
+        })
+        .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime());
       appendMessages(messagesFirestore);
     });
     return () => unsubscribe();
@@ -104,6 +120,32 @@ export default function Chat() {
     await Promise.all(writes);
   }
 
+  async function sendImage(uri, roomPath) {
+    const { url, fileName } = await uploadImage(
+      uri,
+      `images/rooms/${roomPath || roomHash}`
+    );
+    const message = {
+      _id: fileName,
+      text: "",
+      createdAt: new Date(),
+      user: senderUser,
+      image: url,
+    };
+    const lastMessage = { ...message, text: "Image" };
+    await Promise.all([
+      addDoc(roomMessagesRef, message),
+      updateDoc(roomRef, { lastMessage }),
+    ]);
+  }
+
+  async function handlePhotoPicker() {
+    const result = await pickImage();
+    if (!result.cancelled) {
+      await sendImage(result.uri);
+    }
+  }
+
   return (
     <ImageBackground
       resizeMode="cover"
@@ -115,6 +157,111 @@ export default function Chat() {
         messages={messages}
         user={senderUser}
         renderAvatar={null}
+        renderActions={(props) => (
+          <Actions
+            {...props}
+            containerStyle={{
+              position: "absolute",
+              right: 50,
+              bottom: 5,
+              zIndex: 9999,
+            }}
+            onPressActionButton={handlePhotoPicker}
+            icon={() => (
+              <Ionicons
+                name="camera"
+                size={30}
+                color={colors.iconGray}
+              ></Ionicons>
+            )}
+          ></Actions>
+        )}
+        timeTextStyle={{ right: { color: colors.iconGray } }}
+        renderSend={(props) => {
+          const { text, messageIDGenerator, user, onSend } = props;
+          return (
+            <TouchableOpacity
+              style={{
+                height: 40,
+                width: 40,
+                borderRadius: 40,
+                backgroundColor: colors.primary,
+                alignItems: "center",
+                justifyContent: "center",
+                marginBottom: 5,
+              }}
+              onPress={() => {
+                if (text && onSend) {
+                  onSend({
+                    text: text.trim(),
+                    user,
+                    _id: messageIDGenerator(),
+                  });
+                }
+              }}
+            >
+              <Ionicons name="send" size={20} color={colors.white}></Ionicons>
+            </TouchableOpacity>
+          );
+        }}
+        renderInputToolbar={(props) => (
+          <InputToolbar
+            {...props}
+            containerStyle={{
+              marginLeft: 10,
+              marginRight: 10,
+              marginBottom: 2,
+              borderRadius: 20,
+              paddingTop: 5,
+            }}
+          ></InputToolbar>
+        )}
+        renderBubble={(props) => (
+          <Bubble
+            {...props}
+            textStyle={{ right: { color: colors.text } }}
+            wrapperStyle={{
+              left: {
+                backgroundColor: colors.white,
+              },
+              right: {
+                backgroundColor: colors.tertiary,
+              },
+            }}
+          ></Bubble>
+        )}
+        renderMessageImage={(props) => {
+          return (
+            <View style={{ borderRadius: 15, padding: 2 }}>
+              <TouchableOpacity
+                onPress={() => {
+                  setModalVisible(true);
+                  setSelectedImageView(props.currentMessage.image);
+                }}
+              >
+                <Image
+                  resizeMode="contain"
+                  style={{
+                    width: 200,
+                    height: 200,
+                    padding: 6,
+                    borderRadius: 15,
+                    resizeMode: "cover",
+                  }}
+                  source={{ uri: props.currentMessage.image }}
+                ></Image>
+                {selectedImageView ? (
+                  <ImageView
+                    imageIndex={0}
+                    visible={modalVisible}
+                    onRequestClose={() => setModalVisible(false)}
+                    images={[{ uri: selectedImageView }]}
+                  ></ImageView>
+                ) : null}
+              </TouchableOpacity>
+            </View>
+          );
+        }}
       ></GiftedChat>
     </ImageBackground>
   );
