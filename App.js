37f20ef025fa48ddd3aab0729ef8ace1ec68de5e
chinfanster <chinfanster@gmail.com>
2022-09-02 13:10:42 -0700

    successfully render signin screen

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, LogBox } from "react-native";
+import { useAssets } from "expo-asset";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ContextWrapper from "./context/ContextWrapper";
+import Context from "./context/Context";
+import SigninScreen from "./screens/SigninScreen";
 
-export default function App() {
+LogBox.ignoreLogs([
+  "Setting a timer",
+  "AsyncStorage has been extracted from react-native core and will be removed in a future release.",
+]);
+
+const Stack = createStackNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Signin" component={SigninScreen}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
+
+function Main() {
+  const [assets] = useAssets(
+    require("./assets/icon-square.png"),
+    require("./assets/chatbg.png"),
+    require("./assets/user-icon.png"),
+    require("./assets/welcome-img.png")
+  );
+  if (!assets) {
+    return <Text>Loading ..</Text>;
+  }
+  return (
+    <ContextWrapper>
+      <App></App>
+    </ContextWrapper>
+  );
+}
+
+export default Main;
---
+++ firebase.js
@@ -0,0 +1,31 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { getStorage } from "firebase/storage";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBlYEAHVWYmDghYf1sHWap9c8UFAKlSYNc",
+  authDomain: "wp-clone-256aa.firebaseapp.com",
+  projectId: "wp-clone-256aa",
+  storageBucket: "wp-clone-256aa.appspot.com",
+  messagingSenderId: "979935378743",
+  appId: "1:979935378743:web:c2ca33520c51cc8dbdaeb2",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
+
+export function signIn(email, password) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
+export function signUp(email, password) {
+  return createUserWithEmailAndPassword(auth, email, password);
+}
